b8231255105ecfe4d29270fe679d28d64741cfb9
SukriTM <micsu.ky@gmail.com>
2023-02-28 01:12:07 +0700

    Lab17 completed

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,50 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *dPtr,int N,int M){
+	cout << fixed << setprecision(2);
+	for (int i = 0; i < N*M; i++)
+	{
+		cout << dPtr[i];
+		if(i%M==M-1) cout << endl;
+		else cout << " ";
+	}
+	
+}
+
+void randData(double *dPtr,int N,int M){
+	for (int i = 0; i < N*M; i++)
+	{
+		dPtr[i]=(double)(rand()%101)/100;
+	}
+	
+}
+
+void findRowSum(const double *dPtr, double *sum1 ,int N ,int M){
+	for (int i = 0; i < N; i++)
+	{
+		double sumRow = 0;
+		for (int j = 0; j < M; j++)
+		{
+			sumRow += dPtr[i*M+j];
+			sum1[i]=sumRow;
+		}
+
+	}
+	
+}
+
+void findColSum(const double *dPtr, double *sum2 , int N,int M){
+	for (int i = 0; i < M; i++)
+	{
+		double sumCol = 0;
+		for (int j = 0; j < N; j++)
+		{
+			sumCol += dPtr[j*M+i];
+			sum2[i]=sumCol;
+		}
+
+	}
+	
+}
